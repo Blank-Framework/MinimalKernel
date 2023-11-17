3954d98318b19d4a706883b9b5a7ae7139cf0d5a
Adrian Du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2023-11-17 19:45:19 +0100

    BF-42 (Added) MinimalControllerInterface
    Interface for the required Controller class to make the kernel work.

---
+++ src/Interface/MinimalControllerInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BlankFramework\MinimalKernel\Interface;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+interface MinimalControllerInterface
+{
+    public function get(ServerRequestInterface $request): ?ResponseInterface;
+    public function post(ServerRequestInterface $request): ?ResponseInterface;
+    public function put(ServerRequestInterface $request): ?ResponseInterface;
+    public function patch(ServerRequestInterface $request): ?ResponseInterface;
+    public function delete(ServerRequestInterface $request): ?ResponseInterface;
+    public function connect(ServerRequestInterface $request): ?ResponseInterface;
+    public function options(ServerRequestInterface $request): ?ResponseInterface;
+    public function trace(ServerRequestInterface $request): ?ResponseInterface;
+}
