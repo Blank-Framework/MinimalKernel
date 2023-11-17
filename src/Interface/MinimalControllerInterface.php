81a2d85263ec6ea79891f23583f3dedadecae4bc
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2023-11-17 19:44:44 +0100

    Fixed request interface needed

--- src/Interface/MinimalControllerInterface.php
+++ src/Interface/MinimalControllerInterface.php
@@ -2,17 +2,17 @@
 
 namespace BlankFramework\MinimalKernel\Interface;
 
-use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 interface MinimalControllerInterface
 {
-    public function get(RequestInterface $request): ?ResponseInterface;
-    public function post(RequestInterface $request): ?ResponseInterface;
-    public function put(RequestInterface $request): ?ResponseInterface;
-    public function patch(RequestInterface $request): ?ResponseInterface;
-    public function delete(RequestInterface $request): ?ResponseInterface;
-    public function connect(RequestInterface $request): ?ResponseInterface;
-    public function options(RequestInterface $request): ?ResponseInterface;
-    public function trace(RequestInterface $request): ?ResponseInterface;
+    public function get(ServerRequestInterface $request): ?ResponseInterface;
+    public function post(ServerRequestInterface $request): ?ResponseInterface;
+    public function put(ServerRequestInterface $request): ?ResponseInterface;
+    public function patch(ServerRequestInterface $request): ?ResponseInterface;
+    public function delete(ServerRequestInterface $request): ?ResponseInterface;
+    public function connect(ServerRequestInterface $request): ?ResponseInterface;
+    public function options(ServerRequestInterface $request): ?ResponseInterface;
+    public function trace(ServerRequestInterface $request): ?ResponseInterface;
 }
